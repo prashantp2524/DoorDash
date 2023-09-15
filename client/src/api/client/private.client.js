57ce249776b5e253383a02ae632aa490ad9c3d45
Prashant Panchal <prashantp2524@gmail.com>
2023-09-15 13:00:33 +0530

    vercel link

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "door-dash-seven.vercel.app/api/v1/";
+const baseURL = "https://door-dash-seven.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
